d6030e15eb99496b3e7cb77ad5cd0961f210cb4c
Adwaith Prakash <adwaithprakash03@gmail.com>
2025-04-07 22:25:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,26 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContent = document.querySelectorAll('[data-content]')
+      
+      tabs.forEach(tab=>{
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
 
+            tabsContent.forEach(tabContents =>{
+                tabContents.classList.remove('skills__active')
+            })
+
+            target.classList.add('skills__active')
+
+            tabs.forEach(tab=>{
+                tab.classList.remove('skills__active')
+            })
+           
+            tab.classList.add('skills__active')
+        })
+      })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
